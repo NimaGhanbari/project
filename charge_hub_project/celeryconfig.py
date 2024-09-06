1a72cdb16af9fb1b9f65f299b22fb10e66f329d5
nima ghanbari <nimagithub1401@gmail.com>
2024-09-06 18:09:23 +0330

    edit celery config file

--- charge_hub_project/celeryconfig.py
+++ charge_hub_project/celeryconfig.py
@@ -1,5 +1,5 @@
-broker_url = 'redis://localhost'
-result_backend = 'redis://localhost'
+broker_url = 'redis://redis:6379/0'
+result_backend = 'redis://redis:6379/0'
 task_serializer = 'json'
 result_serializer = 'json'
 accept_content = ['json']
